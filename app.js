f3ea2e5dcf289835ebb76762103d799ad5bd2d1a
Adebayo Aishat <aishatadebayor0@gmail.com>
2022-07-08 23:07:55 -0700

    'ongoing'

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 //Onclick Function for the nav link
 let a= document.getElementById("featCard");
 let b= document.getElementById("compCard");
+let menu= document.querySelector('.nav');
+let open= document.querySelector('.menu-logo');
+let close= document.querySelector('.close-menu');
     
-function myFunction(){
+function showCard1(){
     b.style.display='none';
  if (a.classList.contains('d-none')){
     a.style.display='block';
@@ -15,7 +18,7 @@ function myFunction(){
  };
 
 };
-function myFunction2(){
+function showCard2(){
   a.style.display='none';
     if (b.classList.contains('d-none')){
        b.style.display='block';
@@ -28,3 +31,35 @@ function myFunction2(){
     };
     
    };
+
+   function myMenu(){
+   menu.style.display='block';   
+   close.style.display='block';
+   }
+   function closeMenu(){
+      menu.style.display='none';   
+      close.style.display='none';
+      }
+
+
+
+
+      /* When the user clicks on the button,
+toggle between hiding and showing the dropdown content 
+function showCard1() {
+   document.getElementById("myDropdown").classList.toggle("show");
+ }
+ 
+ // Close the dropdown menu if the user clicks outside of it
+ window.onclick = function(event) {
+   if (!event.target.matches('.dropbtn')) {
+     var dropdowns = document.getElementsByClassName("dropdown-content");
+     var i;
+     for (i = 0; i < dropdowns.length; i++) {
+       var openDropdown = dropdowns[i];
+       if (openDropdown.classList.contains('show')) {
+         openDropdown.classList.remove('show');
+       }
+     }
+   }
+ }*/
